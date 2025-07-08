1b5c4fd32577d9207b45e2930c53a87c86d9c1b5
Yaniv Bohbot <yanivbohbot5@gmail.com>
2025-07-08 18:49:20 +0300

    login logic

--- src/auth/authentication/authentication.controller.ts
+++ src/auth/authentication/authentication.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDTO } from 'src/users/dto/create_user_dto';
+import { User } from 'src/users/user.entity';
+import { UsersService } from 'src/users/users/users.service';
 
 @Controller('authentication')
-export class AuthenticationController {}
+export class AuthenticationController {
+  constructor(private userService: UsersService) {}
+
+  @Post('Signup')
+  signup(@Body() userDTO: CreateUserDTO): Promise<User> {
+    return this.userService.create(userDTO);
+  }
+
+  @Post('login')
+  login(
+    @Body()
+    loginDTO: LoginDTO,
+  ) {
+    return this.authService.login(loginDTO);
+  }
+}
--- src/auth/authentication/authentication.service.ts
+++ src/auth/authentication/authentication.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
 
+import { LoginDTO } from '../dto/LoginDTO';
+import { User } from 'src/users/user.entity';
+import { UsersService } from 'src/users/users/users.service';
+
 @Injectable()
-export class AuthenticationService {}
+export class AuthenticationService {
+  constructor(private userService: UsersService) {}
+
+  async login(loginDTO: LoginDTO): Promise<User> {
+    const user = await this.userService.findOne(loginDTO); // 1.
+
+    const passwordMatched = await bcrypt.compare(
+      loginDTO.password,
+      user.password,
+    ); // 2.
+
+    if (passwordMatched) {
+      //3
+      delete user.password; // 4.
+      return user;
+    } else {
+      throw new UnauthorizedException('Password does not match'); // 5.
+    }
+  }
+}
