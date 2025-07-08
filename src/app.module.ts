eafdadf2d20547ba7691d84e42c6861e44ce75ac
Yaniv Bohbot <yanivbohbot5@gmail.com>
2025-07-08 18:24:02 +0300

    update the song module and app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,17 +10,30 @@ import { SongsModule } from './songs/songs.module';
 import { LoggerMiddleware } from './common/middlewares/logger/logger.middleware';
 import { SongsController } from './songs/songs.controller';
 import { DevConfigService } from './common/providers/DEvConfigService';
+import { Song } from './songs/song.entity';
+import { Artist } from './artists/artists.entity';
+import { User } from './users/user.entity';
+import { Playlist } from './playlist/playlist.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PlaylistModule } from './playlist/playlist.module';
 
 @Module({
-  imports: [SongsModule],
-  controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: DevConfigService,
-      useClass: DevConfigService,
-    },
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: 'spotify-clone',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      entities: [Song, Artist, User, Playlist],
+      synchronize: true,
+    }),
+    SongsModule,
+    PlaylistModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
