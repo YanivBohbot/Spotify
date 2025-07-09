0b9e32c4d02a85595fefc0b1732d30028b636bdf
Yaniv Bohbot <yanivbohbot5@gmail.com>
2025-07-09 12:19:58 +0300

    more logic

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,11 +2,23 @@ import { Module } from '@nestjs/common';
 import { AuthenticationController } from './authentication/authentication.controller';
 import { AuthenticationService } from './authentication/authentication.service';
 import { UsersModule } from 'src/users/users/users.module';
-
+import { JwtStrategy } from './authentication/jwt-strategy';
+import { authConstants } from './authentication/auth.contant';
+import { JwtModule } from '@nestjs/jwt';
+import { ArtistModule } from 'src/artist/artist.module';
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: authConstants.secret,
+      signOptions: {
+        expiresIn: '1d',
+      },
+    }),
+    ArtistModule,
+  ],
   exports: [AuthenticationService],
   controllers: [AuthenticationController],
-  providers: [AuthenticationService],
+  providers: [AuthenticationService, JwtStrategy],
 })
 export class AuthModule {}
